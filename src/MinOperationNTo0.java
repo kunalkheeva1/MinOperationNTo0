79bd815db669039268914ee997996a8ac03ea0ee
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-14 23:16:46 -0700

    coded a method that returns the number of operations to make n from 0, condition is we can only double the number of add one into it

--- src/MinOperationNTo0.java
+++ src/MinOperationNTo0.java
@@ -1,6 +1,20 @@
 public class MinOperationNTo0 {
 
-    
+    static int minOperationNTo0(int n){
+        int operation =0;
+
+        while(n>0){
+            if(n%2 ==0){
+                n=n/2;
+                operation++;
+            }
+            else{
+                n-=1;
+                operation++;
+            }
+        }return operation;
+
+    }
 
     public static void main(String[] args) {
 
