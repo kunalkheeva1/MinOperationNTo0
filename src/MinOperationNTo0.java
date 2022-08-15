5b2613f47b06034d0b77bcad1179844c70d89534
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-14 23:25:46 -0700

    added comments for the better understandings

--- src/MinOperationNTo0.java
+++ src/MinOperationNTo0.java
@@ -1,8 +1,15 @@
 public class MinOperationNTo0 {
 
+
+    // making a number n from zero, condition is i can only double from 0 or add 1 in it
+    // but its not making sense to me, as I just need the number of operations form 0 -n
+    // Now what I can do is half the n and decrement 1 to reach 0
+    //condition will be, half only when n is even and -1 otherwise
     static int minOperationNTo0(int n){
         int operation =0;
 
+        //apply the operations until it becomes 0
+        // and then return the number of operations
         while(n>0){
             if(n%2 ==0){
                 n=n/2;
